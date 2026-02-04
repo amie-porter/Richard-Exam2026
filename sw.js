a1e2f68b73aad161ae8ed40009941d43b49ae804
amie-porter <amieporter@gmail.com>
2026-02-04 20:27:21 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker for Exam Assistant
+const CACHE_NAME = 'exam-assistant-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Add other files you use (CSS, JS, images)
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
